083f3e15ad824b3b00671bd5e91a557c661e3228
ctxzz <ctxs19@gmail.com>
2018-11-14 15:46:05 +0900

    add tagTablewView

--- personalized-care-planner/ViewController/AddModalViewController.swift
+++ personalized-care-planner/ViewController/AddModalViewController.swift
@@ -8,18 +8,33 @@
 
 import Foundation
 import UIKit
+import Material
+
+struct Tag {
+//    let user: User
+    let color: UIColor
+    let category: String
+    let description: String
+}
 
 class AddModalViewController: UIViewController {
     var leftNavigationItems: [UIBarButtonItem]!
-
+    var rightNavigationItems: [UIBarButtonItem]!
+    let section = ["Tag Detail"]
+    let items = [["Category", "Color", "Description"]]
+    var tableView: UITableView!
+    var colorList = ["Red", "Blue", "Gray"]
+    var categoryList = ["personality", "relation", "story", "goal"]
+    var tag: Tag!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Add Annotation"
         view.backgroundColor = .white
         
         prepareNavigationBar()
+        prepareTableView()
     }
-    
 }
 
 extension AddModalViewController {
@@ -29,12 +44,52 @@ extension AddModalViewController {
         let closeButton = UIBarButtonItem.init(barButtonSystemItem: .cancel, target: self, action: #selector(closeModalView))
         leftNavigationItems.append(closeButton)
         navigationItem.leftBarButtonItems = leftNavigationItems
+        
+        /// RIGHT Navigation
+        rightNavigationItems = []
+        let saveButton = UIBarButtonItem.init(barButtonSystemItem: .save, target: self, action: #selector(saveTag))
+        rightNavigationItems.append(saveButton)
+        navigationItem.rightBarButtonItems = rightNavigationItems
     }
-
+    
+    internal func prepareTableView() {
+        tableView = UITableView.init(frame: self.navigationController!.view.frame, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view.addSubview(tableView)
+    }
 }
 
 extension AddModalViewController {
-    @objc func closeModalView() -> () {
+    @objc func closeModalView() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func saveTag() {
+        //
+    }
+}
+
+extension AddModalViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        cell.textLabel?.text = self.items[indexPath.section][indexPath.row]
+        cell.selectionStyle = .blue
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.section[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.section.count
     }
 }
